c09b004eb090b091943f1a98c5bdb190e949fd3b
MarkSant <Santana.marco.a@gmail.com>
2020-03-31 10:37:23 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
 import java.util.*;
+import boardgame.*;
 
 public class Program
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 		
 	}
